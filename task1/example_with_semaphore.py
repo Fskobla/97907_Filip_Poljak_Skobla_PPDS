72e6445ef3ea76d635174d52cf54760a66463afc
Fskobla <72413168+Fskobla@users.noreply.github.com>
2022-02-28 19:52:52 +0100

    feat(semaphore): create example of semaphore

---
+++ task1/example_with_semaphore.py
@@ -0,0 +1,33 @@
+from random import randint
+from time import sleep
+from fei.ppds import Thread, Semaphore, Mutex, Event
+from fei.ppds import print
+
+
+class SimpleBarrier:
+    def __init__(self, N):
+        self.N = N
+        self.counter = 0
+        self.mutex = Mutex()
+        self.T = Semaphore(0)
+
+    def wait(self):
+        self.mutex.lock()
+        self.counter += 1
+        if self.counter == self.N:
+            self.counter = 0
+            self.T.signal(self.N)
+        self.mutex.unlock()
+        self.T.wait()
+
+
+def barrier_example(barrier, thread_id):
+    sleep(randint(1, 10)/10)
+    print("vlakno %d pred barierou" % thread_id)
+    barrier.wait()
+    print("vlakno %d po bariere" % thread_id)
+
+sb = SimpleBarrier(5)
+
+threads = [Thread(barrier_example, sb, i) for i in range(5)]
+[t.join() for t in threads]
