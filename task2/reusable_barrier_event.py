6e69d6a6f4f2bd708548e822141fcb4efad63d54
Fskobla <72413168+Fskobla@users.noreply.github.com>
2022-03-01 01:49:33 +0100

    style(reusable_barrier): add docstring and comments

--- task2/reusable_barrier_event.py
+++ task2/reusable_barrier_event.py
@@ -5,13 +5,32 @@ from fei.ppds import print
 
 
 class SimpleBarrier:
+    """
+    A class to represent a barrier object
+    """
     def __init__(self, N):
+        """
+        Constructs all the necessary attributes for the barrier object.
+        Parameters
+        ----------
+            N : int
+                number of threads
+            counter : int
+                counter for barrier
+            mutex : Mutex
+                imported mutex from fei.ppds for use in thread parallelism
+            event : Event
+                imported Event from fei.ppds, which is use for signalization
+        """
         self.N = N
         self.counter = 0
         self.mutex = Mutex()
         self.event = Event()
 
     def wait(self):
+        """
+        Method of barrier with using of event
+        """
         self.mutex.lock()
         self.counter += 1
         if self.counter == self.N:
@@ -23,16 +42,32 @@ class SimpleBarrier:
 
 
 def rendezvous(thread_name):
+    """
+    Function for printing before the barrier
+        Parameters:
+                thread_name: name of the thread
+    """
     sleep(randint(1, 10)/10)
     print('rendezvous: %s' % thread_name)
 
 
 def ko(thread_name):
+    """
+    Function for printing after the barrier
+        Parameters:
+                thread_name: name of the thread
+    """
     print('ko: %s' % thread_name)
     sleep(randint(1, 10)/10)
 
 
 def barrier_example(barrier, thread_name):
+    """
+    Function which uses reusable barrier and print function rendezvous and ko
+        Parameters:
+                barrier: barrier class
+                thread_name: name of the thread
+    """
     while True:
         rendezvous(thread_name)
         barrier.wait()
