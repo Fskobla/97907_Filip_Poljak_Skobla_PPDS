a3d533938df75a5b24085721b6e25d4dcc4b7a8d
Fskobla <72413168+Fskobla@users.noreply.github.com>
2022-04-21 17:55:34 +0200

    style(sync): add docstring and comments by PEP8 standard

--- sync_and_async_versions/sync_primes.py
+++ sync_and_async_versions/sync_primes.py
@@ -1,18 +1,30 @@
+"""
+Copyright 2022 Filip Poljak Skobla. All Rights Reserved.
+Licensed to GPLv2 www.gnu.org/licenses/old-licenses/gpl-2.0.html
+Synchronise version of primes
+"""
+
 import time
 import queue
 
 
 def result(student, numbers):
+    """
+        Function that choosing the numbers if it is prime or not
+    """
     while not numbers.empty():
         number = numbers.get()
+        # If student is Matus - 1s time for define number
         if(student == "Matúš"):
             count_time = 1
+        # If student is Milan - 2s count
         else:
             count_time = 2
         start = time.perf_counter()
         time.sleep(count_time)
         end = time.perf_counter() - start
 
+        # Define if number is prime
         for i in range(2, number):
             if(number % i == 0):
                 print(f"Študent {student}: určil číslo {number},"
@@ -26,14 +38,21 @@ def result(student, numbers):
 
 
 def main():
+    """
+        The main function which initialize variables
+    """
+    # Empty queue for numbers
     numbers = queue.Queue()
+    # Add numbers into the queue
     for number in [3, 4, 6, 7, 10, 13, 14, 18, 22, 24, 29, 30]:
         numbers.put(number)
 
+    # Two students
     results = [result("Matúš", numbers), result("Milan", numbers)]
 
     start = time.perf_counter()
 
+    # Loop which will repeat until the queue is not empty
     loop = True
     while loop:
         for r in results:
