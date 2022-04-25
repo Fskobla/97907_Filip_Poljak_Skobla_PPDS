2f6542eed2b0c55571bd408307db5561f66eacf9
Fskobla <72413168+Fskobla@users.noreply.github.com>
2022-04-25 19:11:53 +0200

    style(cuda): add docstring and comments by PEP8 standard

--- cuda/cuda.py
+++ cuda/cuda.py
@@ -1,3 +1,9 @@
+"""
+Copyright 2022 Filip Poljak Skobla. All Rights Reserved.
+Licensed to GPLv2 www.gnu.org/licenses/old-licenses/gpl-2.0.html
+Image process with CUDA
+"""
+
 from __future__ import division
 from numba import cuda
 import numpy
@@ -7,10 +13,14 @@ import imageio as iio
 
 @cuda.jit
 def my_kernel_3D(io_array):
+    """
+        Function of kernel with 3D grid (representing image)
+    """
     x, y, z = cuda.grid(3)
+    # Current image reduce lightness by half
     io_array[x, y, z] = io_array[x, y, z] / 2
 
-
+# Read random image
 data = iio.imread("image.png")
 threadsperblock = data.shape
 blockspergrid_x = math.ceil(data.shape[0] / threadsperblock[0])
@@ -18,4 +28,5 @@ blockspergrid_y = math.ceil(data.shape[1] / threadsperblock[1])
 blockspergrid_z = math.ceil(data.shape[2] / threadsperblock[2])
 blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
 my_kernel_3D[blockspergrid, threadsperblock](data)
+# Write into file the modified image
 iio.imwrite("newimage.png", data)
